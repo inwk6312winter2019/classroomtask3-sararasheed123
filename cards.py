e83e22dce5bafb244b07dee1bcd219f01af4f4f2
sararasheed123 <Sara.Rasheed@dal.ca>
2019-02-04 15:31:23 +0000

    first commit

---
+++ cards.py
@@ -0,0 +1,16 @@
+class card():
+	"""this create a card obj
+suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7',  '8', '9', '10', 'Jack', 'Queen', 'King']
+
+	def_init_(self,rank=0,suite=2)
+		self.rank=rank
+		self.suite=suite
+def_str_(self):
+	return f"the rank is {rank_names[suit.suit]} and the suit is {card.suit_names[self.suit]}"
+
+ace_of_spade=card ()
+print(ace_of_spade)
+
+
+
